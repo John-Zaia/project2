e71d58045b390808852fd78e70ade4cc0aa31c62
John Zaia <john.zaia@outlook.com>
2025-04-15 22:05:36 -0400

    styled the /admin route

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,42 +1,41 @@
 import Link from 'next/link';
 import { deleteSong } from "./actions.js";
+import styles from "./styles.module.css";
 
 export default async function songCollection() {
   const data = await fetch('http://localhost:4000/songs'); 
   const songs = await data.json();
 
   return (
-    <div>
-        <Link href="/admin/create">Songs</Link>
-        <table>
+    <div className={styles.container}>
+        <Link className={styles.link} href="/admin/create">Songs</Link>
+        <table className={styles.table}>
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Length</th>
-                    <th>Genre</th>
-                    <th>Release Date</th>
-                    <th>Delete</th>
-                    <th>Edit</th>
+                    <th className={styles.item}>ID</th>
+                    <th className={styles.item}>Name</th>
+                    <th className={styles.item}>Length</th>
+                    <th className={styles.item}>Genre</th>
+                    <th className={styles.item}>Release Date</th>
+                    <th className={styles.item}>Delete</th>
+                    <th className={styles.item}>Edit</th>
                 </tr>
             </thead>
             {
                 songs.map ( song => (
                     <tbody key={song.id}>
                         <tr>
-                            <td>{song.id}</td>
-                            <td>{song.name}</td>
-                            <td>{song.length}</td>
-                            <td>{song.genre}</td>
-                            <td>{song.releasedate}</td>
-                            <td>
+                            <td className={styles.item}>{song.id}</td>
+                            <td className={styles.item}>{song.name}</td>
+                            <td className={styles.item}>{song.length}</td>
+                            <td className={styles.item}>{song.genre}</td>
+                            <td className={styles.item}>{song.releasedate}</td>
+                            <td className={styles.item}>
                             <form action={deleteSong.bind(null, song.id)}>
-                                <input type="submit" value="D" />
+                                <input type="submit" value="D" className={styles.linkButton}/>
                             </form>
                             </td>
-                            <td><Link href={`/admin/edit/${song.id}`}>E</Link></td>
-                            <td>
-                            </td>
+                            <td className={styles.item}><Link className={styles.link} href={`/admin/edit/${song.id}`}>E</Link></td>
                         </tr>
                     </tbody>
                 ))
